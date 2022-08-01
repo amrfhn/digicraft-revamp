4ab8ed2698309cbb9389e5465b5d866e0ab318de
amrfhn <amirfarhan.jfri@gmail.com>
2022-08-01 23:16:28 +0800

    add image support to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,19 +7,30 @@ module.exports = {
   mode: mode,
   target: target,
 
+  output: {
+    assetModuleFilename: "images/[hash][ext][query]", //store images to folder in dist
+  },
+
   module: {
     rules: [
       {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource", //inline (small sizes images and stored in js) or resource (big size images)
+      },
+      {
         test: /\.(s[ac]|c)ss$/i,
         use: [
-          MiniCSSExtractPlugin.loader,
+          {
+            loader: MiniCSSExtractPlugin.loader,
+            options: { publicPath: "" },
+          },
           "css-loader",
           "postcss-loader",
           "sass-loader",
         ],
       },
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
@@ -28,6 +39,9 @@ module.exports = {
     ],
   },
   plugins: [new MiniCSSExtractPlugin()],
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
   devtool: "source-map",
   devServer: {
     static: "./dist",
